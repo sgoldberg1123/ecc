d34b7fca5d858ef84b1c4db118033c46c76bbd2f
Sam Goers <seg_relict@live.com>
2015-11-21 15:47:33 -0600

    Mostly a new file for some pseudo code and progress tracking.

--- Main.c
+++ Main.c
@@ -9,9 +9,4 @@ int main() {
 	return 0;
 }
 
-//Stuff on Curve25519 - http://cr.yp.to/ecdh.html
-//Wiki - https://en.wikipedia.org/wiki/Curve25519
-/*properties of curve
- * y^2 = x^3 + 486662x^2 + x
- * modulo p = 2^255-19
- */
+
