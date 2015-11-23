4aae7d455bd5e1e3da27fcbaa4b864e8a77944fa
Sam Goers <seg_relict@live.com>
2015-11-23 16:53:31 -0600

    Added Visual studio project

---
+++ src/src/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int dhecc();
+int pointDoubler();
+int getX();
+int getY();
+int getGenerator();
+int fastExponentiation();
+//function for curve? or will curve be absract with its
+////pieces spread though the code?
+
+int main() {
+
+
+	system("pause");
+	return 0;
+}
+
+
+int dhecc(/**/) {
+	//This function is for the Diffy-Hellman Key exchange w/ ECC
+	return 0;
+}
+
+
+int pointDoubler() {
+	//This should be just the equation for doubling a point.
+	return 0;
+}
+
+int getX() {
+	//Given a point, get X?
+	return 0;
+}
+int getY() {
+	//Given a point, get Y?
+	return 0;
+}
+int getGenerator() {
+	//Generate a point that generates the curve.
+	////Does any point work? If not, what qualities does
+	////the point need?
+	return 0;
+}
+int fastExponentiation() {
+	//Multiply a big number fast. Like,
+	//Given 100g, break up the coefficient
+	//into binary, then multiply that by g?
+	//Note: need a little more research on topic.
+	return 0;
+}
