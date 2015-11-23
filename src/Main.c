a1bba1d0229a4a727e3b0dc1b733e216e52c4792
Sam Goers <seg_relict@live.com>
2015-11-23 15:49:56 -0600

    Added fuctions and function calls in Main.c

--- src/Main.c
+++ src/Main.c
@@ -2,11 +2,49 @@
 #include <stdlib.h>
 #include <math.h>
 
+int dhecc();
+int pointDoubler();
+int getX();
+int getY();
+int getGenerator();
+int fastExponentiation();
+//function for curve? or will curve be absract with its
+////pieces spread though the code?
 
 int main() {
 
 
 	return 0;
 }
+
+
+int dhecc(/**/) {
+	//This function is for the Diffy-Hellman Key exchange w/ ECC
+}
+
+
+int pointDoubler() {
+	//This should be just the equation for doubling a point.
+}
+
+int getX() {
+	//Given a point, get X?
+}
+int getY() {
+	//Given a point, get Y?
+}
+int getGenerator() {
+	//Generate a point that generates the curve.
+	////Does any point work? If not, what qualities does
+	////the point need?
+}
+int fastExponentiation() {
+	//Multiply a big number fast. Like,
+	//Given 100g, break up the coefficient
+	//into binary, then multiply that by g?
+	//Note: need a little more research on topic.
+}
+
+
 
 
