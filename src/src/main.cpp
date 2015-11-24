63a56fa6edf856d4b9e8c933187ff4527b5bd007
Sam Goers <seg_relict@live.com>
2015-11-23 18:43:01 -0600

    all files

---
+++ src/src/main.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
+
+
+int main() {
+
+	t
+
+	system("pause");
+	return 0;
+}
+
+
+int dhecc(/**/) {
+	//This function is for the Diffy-Hellman Key exchange w/ ECC
+	return 0;
+}
+
+
+int pointDoubler() {
+	//This should be just the equation for doubling a point.
+	return 0;
+}
+
+int getX() {
+	//Given a point, get X?
+	return 0;
+}
+
+int getY() {
+	//Given a point, get Y?
+	return 0;
+}
+
+int getGenerator() {
+	//Generate a point that generates the curve.
+	////Does any point work? If not, what qualities does
+	////the point need?
+	return 0;
+}
+
+int fastExponentiation() {
+	//Multiply a big number fast. Like,
+	//Given 100g, break up the coefficient
+	//into binary, then multiply that by g?
+	//Note: need a little more research on topic.
+	return 0;
+}
+
+int curve(int a, int b) {
+	//Y^2 = ?
+}
+
+
+
